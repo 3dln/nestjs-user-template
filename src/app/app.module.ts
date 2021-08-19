083882ff80e47f98ef8beeef9fbf33ab89d3907b
Ashkan Ashtiani <ashcan@3dln.com>
2021-08-19 12:41:52 +0430

    typeorm connection to sqlite
    user base entity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,23 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module';
+import { ReportsModule } from '../reports/reports.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
+import { User } from '../users/entities/user.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmCoreModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
 export class AppModule {}
