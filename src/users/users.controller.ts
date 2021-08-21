6ed56c8e27af5e8e432134dd6951a76ea892be72
Ashkan Ashtiani <ashcan@3dln.com>
2021-08-21 17:00:17 +0430

    current user interceptor which feeds the current user decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Session,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/cerate-user.dto';
 import { UsersService } from './users.service';
@@ -16,9 +17,11 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth/auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './entities/user.entity';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
 @Controller('auth')
 @Serialize(UserDto)
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
